622a7815b941c4715afdcb2358c7f408c7483558
Jeff <xwag575@gmail.com>
2024-11-03 16:05:09 +0800

    Add functionality to generate random number.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+function generateRandomNumber(num) {
+    // Gets # from 0 -> num - 1
+    return Math.floor(Math.random() * num)
+}
+
 const messageComponents = {
     signInfo: ['star', 'moon', 'sun', 'comet'],
     fortunePredict: ['terrible luck', 'bad luck', 'ok luck', 'good luck', 'great luck'],
